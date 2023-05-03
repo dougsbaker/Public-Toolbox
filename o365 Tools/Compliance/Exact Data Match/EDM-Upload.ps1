adff2bf571f0f815b943253afac722002836dddd
douglas Baker <dougsbaker@gmail.com>
2023-05-03 09:24:54 -0700

    deploy

---
+++ o365 Tools/Compliance/Exact Data Match/EDM-Upload.ps1
@@ -0,0 +1,3 @@
+cd "C:\Program Files\Microsoft\EdmUploadAgent"
+
+.\EdmUploadAgent.exe /UploadData /DataStoreName DLPTestSchema /DataFile "C:\EDM\EDM-Data.csv" /HashLocation "C:\EDM\Hash" /schema "C:\EDM\DLP-Schema-MSFT.xml"
